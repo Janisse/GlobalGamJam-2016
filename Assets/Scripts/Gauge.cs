2f1ac0c65d0421525a16e403fdcdad44cda6ee63
Janisse <J_clement_42@live.fr>
2016-01-30 10:38:56 +0100

    - Improve Gauge

---
+++ Assets/Scripts/Gauge.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Gauge : MonoBehaviour
+{
+	#region properties
+	[Range(0f, 1f)]
+	public float gaugeValue = 1f;
+	public float[] timeValues = null;
+	public float maxGaugeWidth = 290f;
+	public Transform gaugeContainer = null;
+	public RectTransform gaugeTransform = null;
+	public Image image = null;
+	public GameObject gaugeSeparation = null;
+
+	private List<GameObject> gaugeSeparationDict = new List<GameObject> ();
+	private float totalTime = 0f;
+	private int step = 0;
+	#endregion
+
+	#region HUD Methods
+	internal void CreateGauges()
+	{
+		//Clear
+		foreach(GameObject obj in gaugeSeparationDict)
+		{
+			Destroy(obj);
+		}
+		gaugeSeparationDict.Clear ();
+		step = 0;
+
+		//Compute gauge duration
+		totalTime = 0f;
+		for(int i=0; i<timeValues.Length; i++)
+		{
+			totalTime += timeValues [i];
+		}
+
+		//Instantiate
+		float currentTime = 0f;
+		for(int i=0; i<timeValues.Length-1; i++)
+		{
+			GameObject newGaugeGO = (GameObject)Instantiate(gaugeSeparation);
+			RectTransform gaugeTrsf = newGaugeGO.GetComponent<RectTransform>();
+			gaugeTrsf.SetParent(gaugeContainer);
+			gaugeTrsf.anchorMin = new Vector2 (0f, 0.5f);
+			gaugeTrsf.anchorMax = new Vector2 (0f, 0.5f);
+			gaugeTrsf.pivot = new Vector2 (0f, 0.5f);
+
+			//Formule Separator placement
+			currentTime += + timeValues[i];
+			float placeX = (currentTime / totalTime) * maxGaugeWidth;
+			gaugeTrsf.localPosition = new Vector3 (placeX, 0f, 0f);
+
+			gaugeSeparationDict.Add (newGaugeGO);
+		}
+	}
+
+	internal bool UpdateGauge (float a_time)
+	{
+		float a_gaugeValue = Mathf.Clamp01(a_time / totalTime);
+		gaugeTransform.sizeDelta = new Vector2 (a_gaugeValue * maxGaugeWidth, gaugeTransform.sizeDelta.y);
+
+		//Check if Next Step
+		float value = 0f;
+		for(int i=0; i<timeValues.Length; i++)
+		{
+			value += timeValues [i];
+			if(a_time <= value)
+			{
+				if(step != i)
+				{
+					step = i;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+	#endregion
+}
--- Assets/Scripts/HUDPanel.cs
+++ Assets/Scripts/HUDPanel.cs
@@ -1,70 +1,25 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
-using System.Collections.Generic;
 
 public class HUDPanel : MonoBehaviour
 {
 	#region Properties
-	[Range(0f, 1f)]
-	public float gaugeValue = 1f;
-	public int nbPart = 3;
-	public float maxGaugeWidth = 290f;
-	public Transform gaugeContainer = null;
-	public RectTransform gaugeTransform = null;
-	public Image image = null;
-	public GameObject gaugeSeparation = null;
-
-	private List<GameObject> gaugeSeparationList = null;
+	public float gaugeTime = 1f;
+	public Gauge gauge = null;
 	#endregion
 
 	#region Class Methods
 	// Use this for initialization
 	void Start ()
 	{
-		CreateGauges (nbPart);
+		gauge.CreateGauges ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		UpdateGauge (gaugeValue);	//PlaceHolder
-	}
-	#endregion
-
-	#region HUD Methods
-	internal void CreateGauges(int a_nbPart)
-	{
-		//Clear
-		if(gaugeSeparationList != null)
-		{
-			foreach(GameObject obj in gaugeSeparationList)
-			{
-				Destroy(obj);
-			}
-			gaugeSeparationList.Clear ();
-		}
-		else
-		{
-			gaugeSeparationList = new List<GameObject> ();
-		}
-
-		//Instantiate
-		for(int i=1; i<a_nbPart; i++)
-		{
-			GameObject newGaugeGO = (GameObject)Instantiate(gaugeSeparation);
-			RectTransform gaugeTrsf = newGaugeGO.GetComponent<RectTransform>();
-			gaugeTrsf.SetParent(gaugeContainer);
-			gaugeTrsf.anchorMin = new Vector2 (0f, 0.5f);
-			gaugeTrsf.anchorMax = new Vector2 (0f, 0.5f);
-			gaugeTrsf.pivot = new Vector2 (0f, 0.5f);
-			gaugeTrsf.localPosition = new Vector3 (i * (maxGaugeWidth / a_nbPart), 0f, 0f);
-		}
-	}
-
-	internal void UpdateGauge (float a_gaugeValue)
-	{
-		gaugeTransform.sizeDelta = new Vector2 (a_gaugeValue * maxGaugeWidth, gaugeTransform.sizeDelta.y);
+		bool res = gauge.UpdateGauge (gaugeTime);	//PlaceHolder
+		Debug.Log(res);		//PlaceHolder
 	}
 	#endregion
 }
