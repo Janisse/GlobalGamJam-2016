7fc0a5d69e1696f3e64767f85cc4478525aac30f
Pierre <pierre.biolley@etu.udamail.fr>
2016-01-30 10:46:47 +0100

    ajout scrip spell et wind spell

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class Spell : MonoBehaviour
+{
+	public GameObject Player = null;
+	Sprite Icon = null;
+
+	internal virtual void LaunchEffect ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Wind Spell.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindSpell : Spell
+{
+	internal override void LaunchEffect ()
+	{
+		base.LaunchEffect ();
+	}
+}
