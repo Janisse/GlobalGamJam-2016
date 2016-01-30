704c69cc4d71d59837b02b99dbda728c796736ba
Pierre <pierre.biolley@etu.udamail.fr>
2016-01-30 02:22:34 +0100

    -Ajout de la scene du menu et du premier niveau -avec un bouton !

---
+++ Assets/OnClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPlayClick()
+	{
+		Application.LoadLevel("Level 1");
+	}
+}
