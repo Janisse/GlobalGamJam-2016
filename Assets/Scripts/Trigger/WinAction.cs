3f0e49da3c70d1129af405d9dd606f790552961b
Mhyshka <elunywow@gmail.com>
2016-01-31 17:04:33 +0100

    End Action + Prefab

---
+++ Assets/Scripts/Trigger/WinAction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinAction : TriggerAction
+{
+	public AudioSource soundWin = null;
+	protected bool triggerOnlyOnce = false;
+	#region TriggerAction Methods
+	internal override void OnEnter()
+	{
+		if (!isTrigger && !triggerOnlyOnce)
+		{
+			triggerOnlyOnce = true;
+			soundWin.Play();
+			Invoke ("Win", 2f);
+		}
+		isTrigger = true;
+	}
+		
+	public void Win()
+	{
+		Debug.Log ("Load scene");
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("End", UnityEngine.SceneManagement.LoadSceneMode.Single);
+	}
+	#endregion
+}
