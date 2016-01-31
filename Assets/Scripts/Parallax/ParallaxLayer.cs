6d65ba6646d6a3e30824e3067a891e813bf6bbfb
Mhyshka <elunywow@gmail.com>
2016-01-31 15:08:20 +0100

    Gameplay background prefabs + parallax tweaks.

--- Assets/Scripts/Parallax/ParallaxLayer.cs
+++ Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class ParallaxLayer : MonoBehaviour
 {
 	#region Inspector Properties
-	[Range(0f, 5f)]
+	[Range(-5f, 5f)]
 	public float layerDistanceMultiplierX = 1f;
 
-	[Range(0f, 5f)]
+	[Range(-5f, 5f)]
 	public float layerDistanceMultiplierY = 1f;
+
+	public float minOffsetY = -20f;
+	public float maxOffsetY = 20f;
 	#endregion
 
 	internal void SetParallaxOffset(Vector2 a_offset)
 	{
 		Vector3 position = transform.localPosition;
 		position.x = a_offset.x * (1f - layerDistanceMultiplierX);
-		position.y = a_offset.y * (1f - layerDistanceMultiplierY);
+		position.y = Mathf.Clamp(a_offset.y * (1f - layerDistanceMultiplierY), minOffsetY, maxOffsetY);
 		transform.localPosition = position;
 	}
 }
