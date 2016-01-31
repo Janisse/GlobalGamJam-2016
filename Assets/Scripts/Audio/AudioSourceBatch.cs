920ac561fafe55b97b04e6e6358058b03fe72c8e
Mhyshka <elunywow@gmail.com>
2016-01-31 02:27:16 +0100

    Footstep sounds. Sound batch script ( random + pitch ). Animation event catch.

---
+++ Assets/Scripts/Audio/AudioSourceBatch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioSourceBatch : MonoBehaviour
+{
+    public AudioSource[] sources = null;
+    public Vector2 pitchRange = Vector2.one;
+
+    internal void Play()
+    {
+        List<AudioSource> freeSources = new List<AudioSource>();
+
+        foreach (AudioSource each in sources)
+        {
+            if (!each.isPlaying)
+            {
+                freeSources.Add(each);
+            }
+        }
+
+        if (freeSources.Count > 0)
+        {
+            int index = Random.Range(0, freeSources.Count);
+            freeSources[index].pitch = Random.Range(pitchRange.x, pitchRange.y);
+            freeSources[index].Play();
+        }
+        else
+        {
+            Debug.LogWarning("Audio source batch : no free audio source.");
+        }
+    }
+}
---
+++ Assets/Scripts/Character/CharacterAnimEventCatcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimEventCatcher : MonoBehaviour
+{
+    public AudioSourceBatch footstepAS = null;
+
+	void footstep ()
+    {
+        footstepAS.Play();
+	}
+}
